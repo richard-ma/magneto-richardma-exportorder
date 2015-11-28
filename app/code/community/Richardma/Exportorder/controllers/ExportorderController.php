c16ea3b3bc2c427fc0ea2c2c8f3937b3a5f87b03
Richard Ma <richard.ma.19850509@gmail.com>
2015-11-28 08:17:43 +0800

    init commit

---
+++ app/code/community/Richardma/Exportorder/controllers/ExportorderController.php
@@ -0,0 +1,9 @@
+<?php
+class Richardma_Exportorder_ExportorderController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
