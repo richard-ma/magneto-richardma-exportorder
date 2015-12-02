5fe26a667653c17fcbc23ea4ddf9b6510c94b9ba
Richard Ma <richard.ma.19850509@gmail.com>
2015-12-02 16:22:28 +0800

    add _genOrdersIdList function

--- app/code/community/Richardma/Exportorder/controllers/ExportorderController.php
+++ app/code/community/Richardma/Exportorder/controllers/ExportorderController.php
@@ -14,12 +14,40 @@ class Richardma_Exportorder_ExportorderController extends Mage_Adminhtml_Control
 
     public function exportAction() 
     {
-        $this->loadLayout();
+        $orderIds = $this->getRequest()->getPost('orderIds');
+        //var_dump($this->_genOrdersIdList($orderIds));
 
-        $block = $this->getLayout()->createBlock('richardma_exportorder/template')
-            ->setTemplate('export.phtml');
-        $this->_addContent($block);
+        //$orders = Mage::getModel('sales/order')->getCollection();
+        //$orders->addAttributeToFilter('id', 1);
+        $orders = Mage::getModel('sales/order')->load('100000001')->getCustomerFirstname();
+        var_dump($orders);
+        //$orders->load();
 
-        $this->renderLayout();
+        //foreach ($orders as $_order) {
+            //var_dump($_order->getData());
+        //}
+    }
+
+    private function _genOrdersIdList($post_string) 
+    {
+        $temp_list = split(',', $post_string);
+        $ret_list = array();
+        foreach($temp_list as $id) {
+            $id = trim($id);
+            if (strpos($id, '-') != false) {
+                $range = split('-', $id);
+                $start = $range[0];
+                $end = $range[1];
+                for ($i = $start; $i <= $end; $i++) {
+                    array_push($ret_list, $i);
+                }
+            } else {
+                array_push($ret_list, $id);
+            }
+        }
+        $ret_list = array_unique($ret_list);
+        sort($ret_list);
+
+        return $ret_list;
     }
 }
